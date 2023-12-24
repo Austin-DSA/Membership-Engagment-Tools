1cf346c1091a6a62243c7f65496acc13be09962b
Wil Thieme <wil.thieme@protonmail.com>
2023-12-24 15:05:55 -0500

    docstrings for emailapi

--- ListManagement/EmailAPI.py
+++ ListManagement/EmailAPI.py
@@ -1,3 +1,10 @@
+"""
+Provides classes and exceptions for downloading zip attachments from an IMAP email account.
+
+For more information, refer to the documentation of the individual classes and methods.
+"""
+
+
 import imaplib
 import email
 import email.utils
@@ -11,30 +18,68 @@ import dataclasses
 
 
 class Constants:
+    """Contains constants used for email communication."""
     GMAIL_HOST = "imap.gmail.com"
     GMAIL_SMTP_HOST = "smtp.gmail.com"
     GMAIL_SMTP_PORT = 465
 
     class Headers:
+        """Contains constants used in the email headers."""
         DATE = "Date"
 
     class Responses:
+        """Contains constants used in determining email server responses."""
         OK = "OK"
 
 
 class EmailApiException(Exception):
+    """Exception class for custom email API exceptions."""
+    class NoUnread(Exception):
+        """Exception class to be raised when no unread email is found."""
+        pass
     class NoUnreadRecentEnough(Exception):
+        """Exception class to be raised when no unread email is found that is more recent than the requested date."""
+        pass
+    class NoAttachmentMatch(Exception):
+        """Exception class to be raised when the email doesn't have a attachment matching the specified criteria."""
         pass
 
 
 @dataclasses.dataclass
 class Attachement:
+    """
+    A class representing an email attachment.
+
+    Attributes:
+        path (str): The path to the attachment file.
+        name (str): The name of the attachment.
+
+    """
     path: str
     name: str
 
 
 class EmailAccount:
+    """
+    A class representing an email account.
+
+    Attributes:
+        username (str): The username of the email account.
+        password (str): The password of the email account.
+        host (str): The host of the email server. Default is Constants.GMAIL_HOST.
+        mail (imaplib.IMAP4_SSL): The IMAP4_SSL object for interacting with the email server.
+        address (str): The email address associated with the account.
+        lastReturnedMessage: The last returned message from the email server.
+    """
     def __init__(self, username: str, password: str, host: str = Constants.GMAIL_HOST) -> None:
+        """Configures IMAP settings and logs in to the email server.
+
+        Args:
+            username (str): The username of the email account.
+            password (str): The password of the email account.
+            host (str, optional): The host of the email server. Default is Constants.GMAIL_HOST.
+
+        """
         self.mail = imaplib.IMAP4_SSL(host)
         self.mail.login(username, password)
         self.mail.select("INBOX", readonly=False)
@@ -43,6 +88,7 @@ class EmailAccount:
         self.lastReturnedMessage = None
 
     def __del__(self):
+        """Cleanly logs out of the email server when exiting."""
         self.mail.logout()
         # self.smtp.quit()
 
@@ -69,9 +115,19 @@ class EmailAccount:
             return None
 
     def _markMessageAsRead(self, message):
+        """Marks a specific email as unread."""
         self.mail.store(message[0], "+FLAGS", "\\Seen")
 
     def _downloadAttachment(self, message, downloadPath, expectedFileName):
+        """
+        Download an attachment from a specific email and save it to the specified path.
+
+        Raises:
+            EmailApiException.NoAttachmentMatch: If no attachment was found that matches the specified file name.
+
+        Returns:
+            None
+        """
         for part in message[1].walk():
             if part.get_content_maintype() == "multipart":
                 continue
@@ -81,18 +137,36 @@ class EmailAccount:
             if expectedFileName is not None:
                 filename = part.get_filename()
                 if filename != expectedFileName:
-                    raise EmailApiException(f"Unexpected filename: Got {filename} expected {expectedFileName}")
+                    raise EmailApiException.NoAttachmentMatch(f"Unexpected filename: Got {filename} expected {expectedFileName}")
             with open(downloadPath, "wb") as fp:
                 fp.write(part.get_payload(decode=True))
 
     def markDownloadedEmailAsUnread(self):
+        """Marks the last returned email as unread. Typically called after downloading and processing an email."""
         if self.lastReturnedMessage:
             self.mail.store(self.lastReturnedMessage[0], "-FLAGS", "\\Seen")
 
-    def downloadZipAttachmentFromMostRecentUnreadEmail(self, fromAddress, subjectContaining, downloadPath, afterDate=None, expectedFileName=None):
+    def downloadZipAttachmentFromMostRecentUnreadEmail(self, fromAddress: str, subjectContaining: str, downloadPath: str, afterDate=None, expectedFileName=None):
+        """
+        Downloads the zip attachment from the most recent unread email that matches the specified criteria.
+
+        Args:
+            fromAddress (str): The email address from which the email should be received.
+            subjectContaining (str): A string that must be found in the subject of the email.
+            downloadPath (str): The path where the downloaded zip file should be saved.
+            afterDate (datetime.datetime, optional): Only consider emails received after this date. Defaults to None.
+            expectedFileName (str, optional): The expected name of the zip file. Defaults to None.
+
+        Raises:
+            EmailApiException.NoUnread: If no unread message is found.
+            EmailApiException.NoUnreadRecentEnough: If the only unread messages found are not recent enough.
+
+        Returns:
+            None
+        """
         message = self._getMostRecentUnreadEmailFrom(address=fromAddress, requiresAttachment=True, subjectContaining=subjectContaining)
         if message is None:
-            raise EmailApiException.NoUnreadRecentEnough("No unread message was found")
+            raise EmailApiException.NoUnread("No unread message was found")
         if afterDate is not None:
             messageDate = datetime.datetime.fromtimestamp(time.mktime(email.utils.parsedate(message[1].get(Constants.Headers.DATE))))
             if messageDate < afterDate:
@@ -100,7 +174,19 @@ class EmailAccount:
         self._downloadAttachment(message=message, downloadPath=downloadPath, expectedFileName=expectedFileName)
         self._markMessageAsRead(message=message)
 
-    def sendMessage(self, toAddress, subject, messageText, attachments: list[Attachement] = None):
+    def sendMessage(self, toAddress: str, subject: str, messageText: str, attachments: list[Attachement] = None):
+        """
+        Sends an email with the specified details.
+
+        Args:
+            toAddress (str): The email address to which the email should be sent.
+            subject (str): The subject line of the email.
+            messageText (str): The content of the email.
+            attachments (list[Attachement], optional): List of attachments to be included in the email. Defaults to None.
+
+        Returns:
+            None
+        """
         message = EmailMessage()
         message.set_content(messageText)
         message["Subject"] = subject
