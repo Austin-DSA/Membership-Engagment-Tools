75b1da805c8b6d8e68254bc9730764e109b57fbd
Wil Thieme <wil.thieme@protonmail.com>
2023-12-24 14:05:57 -0500

    docstrings for validateVote

--- ListManagement/validateVote.py
+++ ListManagement/validateVote.py
@@ -1,3 +1,28 @@
+"""
+Validates votes by comparing them with a membership list. Takes in a vote list CSV file and a membership list CSV file as input, and outputs a CSV file with the validated votes.
+
+Usage:
+    python3 validateVote.py -v <vote-list-csv> -m <membership-list-csv> -o <output-csv>
+
+Arguments:
+    -v, --vote-list: The path to the vote list CSV file.
+    -m, --membership-list: The path to the membership list CSV file.
+    -o, --output: The path to the output CSV file.
+
+The script follows the following steps:
+
+1. Read in the vote list CSV file and extract the necessary columns.
+2. Create Vote objects for each vote entry in the vote list.
+3. Read in the membership list CSV file.
+4. Compare the votes with the membership list to validate them.
+5. Remove any earlier votes from the same person.
+6. Mark the status of each vote as found or not found in the membership list.
+7. Write the validated votes to the output CSV file.
+
+Note: This docstring provides an overview of the script's functionality and usage. Please refer to the code comments for more detailed explanations of each step and component.
+"""
+
+
 import argparse
 import Utils
 import sys
@@ -5,17 +30,21 @@ import dataclasses
 
 
 class Constants:
+    """Contains constant values used in the script."""
     class VOTE_COLS:
+        """Contains constant values for the vote columns."""
         EMAIL = "email"  # Will probably need updating
         VOTE = "vote"
 
     class VOTE_TYPES:
+        """Contains constant values for the vote types."""
         YES = "Yes"
         NO = "No"
         ABSTAIN = "Abstain"
 
 
 class CommmandFlags:
+    """Defines the command line flags and their corresponding long forms."""
     VOTE_LIST = "-v"
     VOTE_LIST_LONG = "--vote-list"
     MEMBERSHIP_LIST = "-m"
@@ -27,12 +56,23 @@ class CommmandFlags:
     USAGE = "usage: python3 proc -v vote-list-csv -m membership-list-csv -o output-csv"
 
     def __init__(self, voteListPath, membershipListPath, outputPath) -> None:
+        """Initializes the CommmandFlags object.
+        Args:
+            voteListPath (str): The path to the vote list CSV file.
+            membershipListPath (str): The path to the membership list CSV file.
+            outputPath (str): The path to the output CSV file.
+        """
         self.voteListPath = voteListPath
         self.mebershipListPath = membershipListPath
         self.outputPath = outputPath
 
 
 def parseArgs():
+    """Parses command line arguments.
+
+    Returns:
+        CommmandFlags: An object containing the parsed command line arguments.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(CommmandFlags.VOTE_LIST, CommmandFlags.VOTE_LIST_LONG, required=True)
     parser.add_argument(CommmandFlags.MEMBERSHIP_LIST, CommmandFlags.MEMBERSHIP_LIST_LONG, required=True)
@@ -43,16 +83,23 @@ def parseArgs():
 
 @dataclasses.dataclass
 class Vote:
+    """Represents a single vote entry."""
     email: str
     vote: str
     status: str = ""
     found: bool = False
 
     def toRow(self):
+        """Converts the Vote object into a list representing a single row for a CSV file.
+
+        Returns:
+            list: A list representing a single row of a CSV file.
+        """
         return [self.email, self.vote, self.status]
 
 
 def main(args):
+    """Hey, I just met you, and this is crazy, but I'm the main function, so call me maybe."""
     flags = parseArgs()
 
     # Read in all the votes
